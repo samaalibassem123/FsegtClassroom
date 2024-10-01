29259f541bc1f00eeb3182e78354b67fa532285e
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-10-01 20:50:31 +0100

    c3

---
+++ components/Cardclass.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Avatar } from "./ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+import { Button } from "./ui/button";
+import { BookText, Video, MessageCircle } from "lucide-react";
+import Link from "next/link";
+
+export default function Cardclass({ classId }) {
+  return (
+    <div className="flex flex-col gap-7 w-[300px] h-[300px] border p-2 rounded-lg bg-white">
+      <div className="flex justify-between items-center  py-2 border-b-2">
+        <h1 className=" font-semibold text-xl text-white bg-black w-fit rounded-lg p-2">
+          2BI
+        </h1>
+        <span className="text-black">Sii mo7ssen</span>
+        {/*<Avatar>
+          <AvatarImage src="/public/user-round.svg" alt="avatar img" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>*/}
+        <div className="size-10 bg-gray-200 rounded-full" />
+      </div>
+      <div>
+        <div className=" text-gray-500 flex gap-2">
+          <span className="underline">Classname:</span>
+          <span className=" no-underline">Math</span>
+        </div>
+        <div className=" text-gray-500 flex gap-2">
+          <span className="underline">Code:</span>
+          <span className=" no-underline select-all">Q67msf</span>
+        </div>
+      </div>
+      <div className="w-full flex justify-center  items-center">
+        <Button>Enter</Button>
+      </div>
+      <div className="flex items-center justify-evenly">
+        <Button asChild className="bg-gray-400">
+          <Link href={`/${classId}/meet`} title="video call">
+            <Video />
+          </Link>
+        </Button>
+        <Button asChild className="bg-orange-300">
+          <Link href={`/${classId}/docs`} title="documents">
+            <BookText />
+          </Link>
+        </Button>
+        <Button asChild className="bg-blue-400">
+          <Link href={`/${classId}/chat`} title="Chat">
+            <MessageCircle />
+          </Link>
+        </Button>
+      </div>
+      <div></div>
+    </div>
+  );
+}
--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,21 +4,45 @@ import { JoinPrompt } from "./JoinPrompt";
 import { motion } from "framer-motion";
 import { ShowAnimation } from "@/utils/animation";
 import { Addprompt } from "./Addprompt";
+import Cardclass from "./Cardclass";
 export default function Dashboard() {
   return (
     <motion.div
       variants={ShowAnimation}
       initial="hidden"
       animate="visible"
-      className="p-5 flex flex-col gap-7"
+      className="p-5 flex flex-col gap-7 "
     >
+      {/*joining a class */}
       <motion.div variants={ShowAnimation} className="flex flex-col gap-6">
         <h1 className="text-3xl font-semibold underline">Join a Class:</h1>
-        <JoinPrompt />
+        <motion.div
+          className="flex gap-6 md:flex-row flex-col items-center "
+          variants={ShowAnimation}
+        >
+          <motion.div variants={ShowAnimation}>
+            <JoinPrompt />
+          </motion.div>
+          <motion.div variants={ShowAnimation}>
+            <Cardclass />
+          </motion.div>
+        </motion.div>
       </motion.div>
+
+      {/*creating a class */}
       <motion.div variants={ShowAnimation} className="flex flex-col gap-6">
-        <h1 className="text-3xl font-semibold underline">Create your Class:</h1>
-        <Addprompt />
+        <h1 className="text-3xl font-semibold underline">Add a Class:</h1>
+        <motion.div
+          className="flex gap-6 md:flex-row flex-col items-center "
+          variants={ShowAnimation}
+        >
+          <motion.div variants={ShowAnimation}>
+            <Addprompt />
+          </motion.div>
+          <motion.div variants={ShowAnimation}>
+            <Cardclass />
+          </motion.div>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
