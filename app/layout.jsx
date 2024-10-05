6a4a7b43f1354ca7506af4f796e7082c44d93a4a
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-10-05 15:28:01 +0100

    c8

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">
+      <body className="dark:bg-white/5">
         <ThemeProvider attribute="class" enableSystem>
           {children}
         </ThemeProvider>
--- components/Uploaddoc.jsx
+++ components/Uploaddoc.jsx
@@ -1,21 +1,50 @@
 "use client";
 
-import { UploadButton } from "@/utils/uploadthing";
 import { UploadDropzone } from "@/utils/uploadthing";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Uploaddoc() {
+  const success = () =>
+    toast.success("Uploaded succesfully", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  const Error = (msg) => {
+    toast.error(msg, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  };
   return (
-    <UploadDropzone
-      endpoint="pdfUploader"
-      onClientUploadComplete={(res) => {
-        // Do something with the response
-        console.log("Files: ", res);
-        alert("Upload Completed");
-      }}
-      onUploadError={(error) => {
-        // Do something with the error.
-        alert(`ERROR! ${error.message}`);
-      }}
-    />
+    <>
+      <UploadDropzone
+        className=" cursor-pointer dark:border-white"
+        endpoint="pdfUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          success();
+        }}
+        onUploadError={(error) => {
+          // Do something with the error.
+          Error(error.message);
+        }}
+      />
+      <ToastContainer />
+    </>
   );
 }
